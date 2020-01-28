c05a60dd91d17da01f1a5bfa5ab9903bbd10cc07
Christine Bumpous <bumpouce@gmail.com>
2020-01-28 16:09:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,15 @@
-# Your Code Here
+def map (array)
+  i = 0 
+  return_array = []
+  
+  while i < array.length do
+    return_array << array[i] * -1
+    i += 1 
+  end
+  
+  return_array
+end
+
+#map([1,2,3,9]) do |number|
+#  number * -1
+#end
