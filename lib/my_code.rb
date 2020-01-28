19077f853f5da79e065cc194722ee1adc418b493
Christine Bumpous <bumpouce@gmail.com>
2020-01-28 17:42:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,7 +16,8 @@ def reduce (array, *start)
   total = 0 
   
   while i < array.length do
-    total += (yield(array[i]))
+    pp "Yields: #{yield(array[i]}"
+#    total += (yield(array[i]))
     i += 1 
   end
   
